e8f73b02519b5def1d6d5d26bdaa4a859b9c33fb
Lucas Machado de Souza <lcsmachaado@gmail.com>
2023-07-20 13:43:27 -0300

    Initial commit

---
+++ src/test/java/dev/lucasmachado/springbootperinity/SpringBootPerinityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lucasmachado.springbootperinity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPerinityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
