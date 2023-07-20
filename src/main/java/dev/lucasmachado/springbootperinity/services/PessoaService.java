7b172e41154de95ebee8f7a7a3d86d39e120e605
Lucas Machado de Souza <lcsmachaado@gmail.com>
2023-07-20 15:32:52 -0300

    findById,save and update methods implemented in PessoaService.java

--- src/main/java/dev/lucasmachado/springbootperinity/services/PessoaService.java
+++ src/main/java/dev/lucasmachado/springbootperinity/services/PessoaService.java
@@ -1,6 +1,8 @@
 package dev.lucasmachado.springbootperinity.services;
 
+import dev.lucasmachado.springbootperinity.entities.Pessoa;
 import dev.lucasmachado.springbootperinity.repositories.PessoaRepository;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,5 +13,20 @@ public class PessoaService {
 
     @Autowired
     private PessoaRepository pessoaRepository;
+
+    public Pessoa findById(Long id) {
+        return pessoaRepository.findById(id).orElseThrow(
+                () -> new ObjectNotFoundException(Pessoa.class, "Categoria com o id : " + id + " nao encontrada.")
+        );
+    }
+
+    public Pessoa save(Pessoa pessoa){
+        return pessoaRepository.save(pessoa);
+    }
+    public Pessoa update(Pessoa pessoa) {
+        findById(pessoa.getId());
+        return pessoaRepository.saveAndFlush(pessoa);
+    }
+
 
 }
