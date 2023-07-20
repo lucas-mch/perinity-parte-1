89023d5e0a005a10dc474208226f83b6c33ffb76
Lucas Machado de Souza <lcsmachaado@gmail.com>
2023-07-20 15:21:16 -0300

    fix JPA relationships @ManyToOne

--- src/main/java/dev/lucasmachado/springbootperinity/entities/Departamento.java
+++ src/main/java/dev/lucasmachado/springbootperinity/entities/Departamento.java
@@ -1,9 +1,12 @@
 package dev.lucasmachado.springbootperinity.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.lucasmachado.springbootperinity.enterprise.AbstractEntity;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.List;
 
 
 //- Cada Departamento terá id, nome.
@@ -12,6 +15,14 @@ import javax.persistence.Table;
 @Table(name = "departamentos")
 public class Departamento extends AbstractEntity {
     private String nome;
+
+    @OneToMany(mappedBy="departamento")
+    @JsonIgnore
+    private List<Pessoa> pessoas;
+
+    @OneToMany(mappedBy="departamento")
+    @JsonIgnore
+    private List<Tarefa> tarefas;
 
     public Departamento() {
     }
