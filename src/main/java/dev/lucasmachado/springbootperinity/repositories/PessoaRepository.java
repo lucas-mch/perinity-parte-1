837a13ba6ab975b642aa642a64f71d401ebf7f95
Lucas Machado de Souza <lcsmachaado@gmail.com>
2023-07-21 01:07:21 -0300

    #repository -> Added Query in PessoaRepository to search Pessoas with departamento and total hours spend in tasks.

--- src/main/java/dev/lucasmachado/springbootperinity/repositories/PessoaRepository.java
+++ src/main/java/dev/lucasmachado/springbootperinity/repositories/PessoaRepository.java
@@ -2,6 +2,13 @@ package dev.lucasmachado.springbootperinity.repositories;
 
 import dev.lucasmachado.springbootperinity.entities.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface PessoaRepository extends JpaRepository<Pessoa,Long> {
+import java.util.List;
+
+public interface PessoaRepository extends JpaRepository<Pessoa,Long>, QuerydslPredicateExecutor<Pessoa> {
+
+    @Query("SELECT p, d, SUM(t.duracao) FROM Pessoa p LEFT JOIN p.tarefas t LEFT JOIN t.departamento d GROUP BY p.id, d.id")
+    List<Pessoa> listarPessoasComDepartamentoETotalHorasGastas();
 }
