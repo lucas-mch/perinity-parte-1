b510075eb598a45f981b045c21197d47624c17c1
Lucas Machado de Souza <lcsmachaado@gmail.com>
2023-07-21 01:02:39 -0300

    Added query to list three task without Pessoa and ordened asc by prazo.

--- src/main/java/dev/lucasmachado/springbootperinity/repositories/TarefaRepository.java
+++ src/main/java/dev/lucasmachado/springbootperinity/repositories/TarefaRepository.java
@@ -2,6 +2,14 @@ package dev.lucasmachado.springbootperinity.repositories;
 
 import dev.lucasmachado.springbootperinity.entities.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface TarefaRepository extends JpaRepository<Tarefa,Long> {
+import java.util.List;
+
+public interface TarefaRepository extends JpaRepository<Tarefa,Long> , QuerydslPredicateExecutor<Tarefa> {
+
+    @Query("SELECT t FROM Tarefa t WHERE t.pessoa IS NULL ORDER BY t.prazo ASC")
+    List<Tarefa> listThreeTaskWithoutPessoaByPrazo();
+
 }
