5622480114644a2fba0d4ff53300e9e73fe2d369
Lucas Machado de Souza <lcsmachaado@gmail.com>
2023-07-21 02:34:00 -0300

    DepartamentoDTO -> Implementacao do metodo findAll para retornar uma lista de DepartamentoDTO

--- src/main/java/dev/lucasmachado/springbootperinity/services/DepartamentoService.java
+++ src/main/java/dev/lucasmachado/springbootperinity/services/DepartamentoService.java
@@ -1,9 +1,13 @@
 package dev.lucasmachado.springbootperinity.services;
 
+import dev.lucasmachado.springbootperinity.dto.DepartamentoDTO;
 import dev.lucasmachado.springbootperinity.repositories.DepartamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -11,5 +15,9 @@ public class DepartamentoService {
 
     @Autowired
     private DepartamentoRepository departamentoRepository;
+
+    public List<DepartamentoDTO> findAll(){
+        return departamentoRepository.findAll().stream().map(DepartamentoDTO::new).collect(Collectors.toList());
+    }
 
 }
