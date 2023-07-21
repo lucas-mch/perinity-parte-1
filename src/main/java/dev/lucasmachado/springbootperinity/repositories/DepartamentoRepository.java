ac25450766a365c54ed5cd3435f9c20fea51e18a
Lucas Machado de Souza <lcsmachaado@gmail.com>
2023-07-21 01:07:56 -0300

    DepartamentoRepository -> Initial implementation

--- src/main/java/dev/lucasmachado/springbootperinity/repositories/DepartamentoRepository.java
+++ src/main/java/dev/lucasmachado/springbootperinity/repositories/DepartamentoRepository.java
@@ -2,8 +2,9 @@ package dev.lucasmachado.springbootperinity.repositories;
 
 import dev.lucasmachado.springbootperinity.entities.Departamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DepartamentoRepository extends JpaRepository<Departamento,Long> {
+public interface DepartamentoRepository extends JpaRepository<Departamento,Long>, QuerydslPredicateExecutor<Departamento> {
 }
