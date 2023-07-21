cde9f2d580d66539d09aca3dd3b4107bb82318d0
Lucas Machado de Souza <lcsmachaado@gmail.com>
2023-07-21 02:34:56 -0300

    PessoaService -> Implementacao do metodo findPessoasByNomeAndPeriod

--- src/main/java/dev/lucasmachado/springbootperinity/services/PessoaService.java
+++ src/main/java/dev/lucasmachado/springbootperinity/services/PessoaService.java
@@ -1,15 +1,20 @@
 package dev.lucasmachado.springbootperinity.services;
 
 import dev.lucasmachado.springbootperinity.dto.PessoaDTO;
+import dev.lucasmachado.springbootperinity.dto.PessoaGastosDTO;
 import dev.lucasmachado.springbootperinity.enterprise.exceptions.DataIntegrityException;
 import dev.lucasmachado.springbootperinity.entities.Pessoa;
+import dev.lucasmachado.springbootperinity.entities.Tarefa;
 import dev.lucasmachado.springbootperinity.repositories.PessoaRepository;
+import dev.lucasmachado.springbootperinity.repositories.TarefaRepository;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,6 +24,9 @@ public class PessoaService {
 
     @Autowired
     private PessoaRepository pessoaRepository;
+
+    @Autowired
+    private TarefaRepository tarefaRepository;
 
     public Pessoa findById(Long id) {
         return pessoaRepository.findById(id).orElseThrow(
@@ -52,6 +60,25 @@ public class PessoaService {
 
     public List<PessoaDTO> listarPessoasComDepartamentoETotalHorasGastas() {
         return pessoaRepository.listarPessoasComDepartamentoETotalHorasGastas().stream().map(PessoaDTO::new).collect(Collectors.toList());
+    }
+
+    public List<PessoaGastosDTO> findPessoasByNomeAndPeriod(String nome, String dataInicio, String dataFim) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        List<Pessoa> pessoas = pessoaRepository.findByNomeContainingIgnoreCase(nome);
+
+        return pessoas.stream()
+                .map(pessoa -> {
+                    List<Tarefa> tarefas = tarefaRepository.findByPessoaAndPrazoBetween(pessoa, LocalDate.parse(dataInicio,formatter), LocalDate.parse(dataFim,formatter));
+
+                    double averageHours = tarefas.stream()
+                            .mapToDouble(Tarefa::getDuracao)
+                            .average()
+                            .orElse(0.0);
+
+                    return new PessoaGastosDTO(pessoa.getId(),pessoa.getNome(), averageHours);
+                })
+                .collect(Collectors.toList());
     }
 
 
